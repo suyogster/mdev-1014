25560dad8266dc1698d5f5664cc6c46d779737b9
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2023-11-07 14:14:47 -0500

    Home screen updated

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,36 +1,36 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function Home({ navigation }: any) {
   return (
     <View style={styles.container}>
       <Button
         color={'red'}
-        title='Layout 1'
+        title='Suyog Shrestha - 200565523 - Layout 1'
         onPress={() => navigation.navigate('Layout1')}
       />
       <Button
         color={'purple'}
-        title='Layout 2'
+        title='Roshan Khatri - 200575702 - Layout 2'
         onPress={() => navigation.navigate('Layout2')}
       />
       <Button
         color={'orange'}
-        title='Layout 3'
+        title='Priyesh Gautam - 200568939 - Layout 3'
         onPress={() => navigation.navigate('Layout3')}
       />
       <Button
         color={'green'}
-        title='Layout 4'
+        title='Chandrika Ghale - 200575692 - Layout 4'
         onPress={() => navigation.navigate('Layout4')}
       />
       <Button
         color={'blue'}
-        title='Layout 5'
+        title='Ripudaman Singh - 200562402 - Layout 5'
         onPress={() => navigation.navigate('Layout5')}
       />
       <Button
         color={'brown'}
-        title='Layout 6'
+        title='Suyog Shrestha - 200565523 - Layout 6'
         onPress={() => navigation.navigate('Layout6')}
       />
     </View>
