1daa3c549a5494db8add89655464b8b3e439273a
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2023-11-07 12:13:32 -0500

    Added Layout 1 / Formatted Code

--- components/Layout1.tsx
+++ components/Layout1.tsx
@@ -1,9 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+
+interface itemType {
+  id: string;
+  image: string;
+}
+
+const renderItem = ({ item }: any) => (
+  <View
+    style={{
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingRight: 2,
+    }}
+  >
+    <Image source={{ uri: item.image }} style={{ width: 150, height: 150 }} />
+    <Text>{item.title}</Text>
+  </View>
+);
 
 export default function Layout1({ navigation }: any) {
+  const [data, setData] = useState<itemType[]>([
+    {
+      id: '1',
+      image:
+        'https://images.unsplash.com/photo-1570661449115-75236441ba48?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: '2',
+      image:
+        'https://plus.unsplash.com/premium_photo-1667842816788-d0e146c9dcf0?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: '3',
+      image:
+        'https://images.unsplash.com/photo-1487360920430-e18a62e59ad2?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'black' }}>Hello</Text>
+      <View style={styles.firstContainer}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+          Layout 1 - Suyog Shrestha
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: '500' }}>200565523</Text>
+      </View>
+      <View style={styles.secondContainer}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          horizontal
+        />
+      </View>
+      <View style={styles.thirdContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert(`You've pressed the first button`)}
+        >
+          <Text>Button 1 pressed</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert(`You've pressed the first button`)}
+        >
+          <Text>Button 2 pressed</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert(`You've pressed the first button`)}
+        >
+          <Text>Button 3 pressed</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,6 +94,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  firstContainer: {
+    flex: 1,
+    backgroundColor: 'lightblue',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: 20,
+    borderRadius: 30,
+  },
+  secondContainer: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
+    alignItems: 'center',
+    width: '100%',
+    borderRadius: 30,
+    padding: 20,
+  },
+  thirdContainer: {
+    flex: 1,
+    backgroundColor: 'silver',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 20,
+    borderRadius: 30,
+    padding: 20,
+  },
+  button: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 5,
   },
 });
--- components/Layout5.tsx
+++ components/Layout5.tsx
@@ -1,60 +1,62 @@
-import React, { useState } from "react";
-import { SafeAreaView, Text, View, StyleSheet,TouchableOpacity, FlatList} from "react-native";
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 
-export default function Flexbox()
-{
-    const [myList,setmyList]=useState([
-        {text:'Morning Walk',key:'1'},
-        {text:'Washing car',key:'2'},
-        {text:'Cook Food',key:'3'},
-        {text:'Do Assignments',key:'4'},
-        {text:'Pay Bills',key:'5'},
-        {text:'Attend Meetings',key:'6'},
-        {text:'Pick-up Order',key:'7'},
-        {text:'Make Shake',key:'8'},
-        {text:'Gyming',key:'9'},
-        {text:'Making Diet',key:'10'},
-        {text:'Watch Netflix',key:'11'},
-        {text:'Sleep',key:'12'},
-    ])
-const onClicking=(key:any)=>{
-    console.log(key)
-}
-    
-    return(
-        <View style = {styles.container}>
-            <Text style = {styles.text}>My List</Text>
-        <View>
+export default function Flexbox() {
+  const [myList, setmyList] = useState([
+    { text: 'Morning Walk', key: '1' },
+    { text: 'Washing car', key: '2' },
+    { text: 'Cook Food', key: '3' },
+    { text: 'Do Assignments', key: '4' },
+    { text: 'Pay Bills', key: '5' },
+    { text: 'Attend Meetings', key: '6' },
+    { text: 'Pick-up Order', key: '7' },
+    { text: 'Make Shake', key: '8' },
+    { text: 'Gyming', key: '9' },
+    { text: 'Making Diet', key: '10' },
+    { text: 'Watch Netflix', key: '11' },
+    { text: 'Sleep', key: '12' },
+  ]);
+  const onClicking = (key: any) => {
+    console.log(key);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My List</Text>
+      <View>
         <FlatList
-        data={myList}
-        renderItem={({item})=>(
-            <TouchableOpacity onPress={()=>onClicking(item.key)}>
-            <Text style = {styles.list}>{item.text}</Text>
+          data={myList}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => onClicking(item.key)}>
+              <Text style={styles.list}>{item.text}</Text>
             </TouchableOpacity>
-        )}></FlatList>
-        </View>
-        </View>
-    )
+          )}
+        ></FlatList>
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'black',
-        paddingHorizontal:20
-    },
-    text:{
-        fontSize:20,
-       textAlign:'center',
-    },
-    list:{
-        padding:30,
-        marginTop:24,
-        paddingTop:16,
-        backgroundColor:'#fff',
-        fontSize:24
-        
-        
-    }
-})
-
- 
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  list: {
+    padding: 30,
+    marginTop: 24,
+    paddingTop: 16,
+    backgroundColor: '#fff',
+    fontSize: 24,
+  },
+});
